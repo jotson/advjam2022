b332c1aae52848f7c3a86fb853f87157cc479543
John Watson <john@watson-net.com>
2022-06-16 07:39:13 -0700

    Fixed pedastal click event

--- game/meaning/book_pedastal.gd
+++ game/meaning/book_pedastal.gd
@@ -8,6 +8,7 @@ func _ready():
 
 
 func _on_StaticBody_input_event(camera, event, position, normal, shape_idx):
+	print(event)
 	if event is InputEventMouseButton:
 		if event.button_index == 1 and event.pressed:
 			emit_signal("clicked")
